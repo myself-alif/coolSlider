8fc7deea3c60d0e16814fda770a50faad54f849b
Al Mahmud Alif <myself.amalif@gmail.com>
2024-08-29 19:17:31 +0600

    project initialization

---
+++ build/blocks/coolSlider/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f9be3de6b5ca7519aac9');
---
+++ build/blocks/coolSlider/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'fab76cd5e3b56acd8a31');
